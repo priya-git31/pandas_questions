10eea723f9ce771984dd86cf2b7646155c6b4f9f
priya-git31 <haripriyagouddr@gmail.com>
2025-01-06 14:35:59 -0500

    Pandas Questions Updated

--- wine_01.py
+++ wine_01.py
@@ -1,4 +1,3 @@
-
 import os
 os.listdir()
 import pandas as pd
@@ -23,8 +22,6 @@ df.loc[3,'chlorides']
 df.loc[4,'alcohol']
 df.loc[0:1, ['chlorides', 'density']]
 df.loc[2:3, ['residual sugar','pH']]
-
-
 ##ILOC 
 df.head()
 
@@ -45,5 +42,24 @@ df.iloc[2:5,3:7]
 df.iloc[[0,2,4],[1,2,5]]
 
 
+# Counting - Count 
+df.head(5)
+# 1. Using count()
+# How many non-null values are there in each column of the dataset?
+# How many non-null values are there in the column quality?
+df.count()
+df['quality'].count()
 
+# 2. Using size
+# What is the total number of elements (rows × columns) in the dataset?
+# How many total elements are present in the first 5 rows of the dataset?
+df.size
+df.loc[0:4].size
 
+#  Using value_counts()
+# How many times does each unique value appear in the quality column?
+# What is the frequency distribution of the alcohol column values in the first 5 rows?
+# Which pH value appears most frequently in the dataset?
+df['quality'].value_counts()
+df.loc[0:4,'alcohol'].value_counts()
+df['pH'].value_counts().max()
